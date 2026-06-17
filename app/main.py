799a0f878e951c7e67492ae233032b86e644086f
Artur Neruk <anerukpfk@gmail.com>
2026-06-17 11:02:20 +0300

    refactor: fix code style according to review changes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 class Person:
     people = {}
 
-    def __init__(self, name: str, age: int):
+    def init(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
         Person.people[name] = self
@@ -10,22 +10,22 @@ class Person:
 def create_person_list(people: list) -> list:
     Person.people.clear()
 
-    person_instances = []
-    for person_dict in people:
-        instance = Person(person_dict["name"], person_dict["age"])
-        person_instances.append(instance)
+
+    person_instances = [
+        Person(p["name"], p["age"]) for p in people
+    ]
+
 
     for person_dict in people:
         current_name = person_dict["name"]
         current_instance = Person.people[current_name]
 
-        if "wife" in person_dict and person_dict["wife"] is not None:
+        if person_dict.get("wife") is not None:
             wife_name = person_dict["wife"]
             current_instance.wife = Person.people[wife_name]
 
-        elif "husband" in person_dict and person_dict["husband"] is not None:
+        elif person_dict.get("husband") is not None:
             husband_name = person_dict["husband"]
             current_instance.husband = Person.people[husband_name]
 
     return person_instances
-
