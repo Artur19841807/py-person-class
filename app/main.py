e2b680ba96723d53680736ee2129281da083ca14
Artur Neruk <anerukpfk@gmail.com>
2026-06-17 10:39:51 +0300

    feat: add person class and create_person_list function

--- app/main.py
+++ app/main.py
@@ -1,8 +1,31 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    Person.people.clear()
+
+    person_instances = []
+    for person_dict in people:
+        instance = Person(person_dict["name"], person_dict["age"])
+        person_instances.append(instance)
+
+    for person_dict in people:
+        current_name = person_dict["name"]
+        current_instance = Person.people[current_name]
+
+        if "wife" in person_dict and person_dict["wife"] is not None:
+            wife_name = person_dict["wife"]
+            current_instance.wife = Person.people[wife_name]
+
+        elif "husband" in person_dict and person_dict["husband"] is not None:
+            husband_name = person_dict["husband"]
+            current_instance.husband = Person.people[husband_name]
+
+    return person_instances
+
